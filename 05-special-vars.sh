aa3b44a640f067ce99bbb6819b8f430e788f2d3b
Rajesh Chandran <rajesh.chandran_clea@INKACTXR1505.cleartax.blr.in>
2022-10-18 15:21:16 +0530

    script changes

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -3,13 +3,13 @@
 ### Special Variables in Linux are : $0 to $n , $* , $@ , $# , $$ 
 
 echo -e "\e[32m Name of the Script: \e[0m"$0   # Prints the script name that you're running
-echo $1   # First argument from the command line
-echo $2   # Second argument from the command line
+echo "First argument: "$1   # First argument from the command line
+echo "Second argument: "$2   # Second argument from the command line
 
-echo $*   # Gives you all the arguments used in the script
-echo $@   # Gives you all the arguments used in the script
-echo $#   # Gves you the number of arguments users
-echo $$   # Gives you the PID of the current process
+echo "All the arguments: "$*   # Gives you all the arguments used in the script
+echo "All the arguments: "$@   # Gives you all the arguments used in the script
+echo "No of arguments: "$#   # Gves you the number of arguments users
+echo "Process ID: "$$   # Gives you the PID of the current process
 
 # On command you can supply 9 variables 
 # Ex  
