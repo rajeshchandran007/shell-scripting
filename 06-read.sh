0936b8d5943cf17eb82a5104fb696ee9c6111a38
Rajesh Chandran <rajesh.chandran_clea@INKACTXR1505.cleartax.blr.in>
2022-10-18 15:48:49 +0530

    script changes

--- 06-read.sh
+++ 06-read.sh
@@ -3,6 +3,8 @@
 read -p 'Enter your name:' NAME 
 echo -e "Name of the executed user is $NAME"
 
+# Another approach given below
+
 echo "What is your Name: "
 read NAME 
 echo "Name of the executed user is $NAME"
