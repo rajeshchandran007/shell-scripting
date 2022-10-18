9a35e123b015d512564c743babd25dd4b005f1bb
Rajesh Chandran <rajesh.chandran_clea@INKACTXR1505.cleartax.blr.in>
2022-10-18 14:55:46 +0530

    script changes

---
+++ 05-special-vars.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+### Special Variables in Linux are : $0 to $n , $* , $@ , $# , $$ 
+
+echo $0   # Prints the script name that you're running
+echo $1   # First argument from the command line
+echo $2   # Second argument from the command line
+
+echo $*   # Gives you all the arguments used in the script
+echo $@   # Gives you all the arguments used in the script
+echo $#   # Gves you the number of arguments users
+echo $$   # Gives you the PID of the current process
+
+# On command you can supply 9 variables 
+# Ex  
+#  sh abc.sh 10 20 30 40
