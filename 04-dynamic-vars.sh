fdfd8a3d23d532fd9d7811e9cfd65524fbe657bc
Rajesh Chandran <rajesh.chandran_clea@INKACTXR1505.cleartax.blr.in>
2022-10-19 07:51:09 +0530

    script changes

--- 04-dynamic-vars.sh
+++ 04-dynamic-vars.sh
@@ -2,7 +2,7 @@
 
 TODAY_DATE="$(date +%F)"
 NO_OF_USERS="$(who | wc -l)"
-X = "$NO_OF_USERS"
+X = "${NO_OF_USERS}"
 
 echo "Good Morning, Todays date is ${TODAY_DATE}"
 echo "Number of sessions opened are : ${NO_OF_USERS}"
