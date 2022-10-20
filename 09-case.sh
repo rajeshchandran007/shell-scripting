71bb2335e985064b2e7f47abf2cfd5d185995f53
Rajesh Chandran <rajesh.chandran_clea@INKACTXR1505.cleartax.blr.in>
2022-10-20 08:25:39 +0530

    script changes

---
+++ 09-case.sh
@@ -0,0 +1,24 @@
+#!/bib/bash
+
+# case $1 in
+
+#     opt1)
+
+#     opt2)
+
+# esac
+
+case $1 in
+
+    start)
+    echo "Start the script" 
+    ;;
+    
+    stop)
+    echo "Stop the script" 
+    ;;
+    *)
+    echo "Give start or stop as the input"
+    
+esac
+
