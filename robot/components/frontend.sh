2a5f5273823d508c67d17312e5f70325a220612a
Rajeshe <rajeshchandran007@gmail.com>
2022-10-21 13:43:40 +0530

    script change

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -10,17 +10,17 @@ if  [ $USERID -ne 0 ] ; then
     exit 1
 fi
 
-yum install nginx -y
+yum install nginx -y &>> /tmp/frontend.log
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
+rm -rf * &>> /tmp/frontend.log
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
-rm -rf frontend-main README.md
+rm -rf frontend-main README.md &>> /tmp/frontend.log
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>> /tmp/frontend.log
+systemctl start nginx &>> /tmp/frontend.log
