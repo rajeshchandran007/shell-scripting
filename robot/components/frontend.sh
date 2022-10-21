c6bb9734ac2407a03d2d1d7798afabc78723fd33
Rajeshe <rajeshchandran007@gmail.com>
2022-10-21 14:37:28 +0530

    script change

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -10,23 +10,24 @@ if  [ $USERID -ne 0 ] ; then
     exit 1
 fi
 
+stat() {
+
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m Success\e[0m"
+    else
+        echo -e "\e[31m Fail\e[0m"
+    fi
+}
+
 echo -n "Installing nginx:"
 yum install nginx -y &>> /tmp/frontend.log
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success\e[0m"
-else
-    echo -e "\e[31m Fail\e[0m"
-fi
+stat $?
 
 echo -n "Downloading project components:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success\e[0m"
-else
-    echo -e "\e[31m Fail\e[0m"
-fi
+stat $?
 
 echo -n "Performing cleanup:"
 cd /usr/share/nginx/html
@@ -37,18 +38,10 @@ mv static/* .
 rm -rf frontend-main README.md &>> /tmp/frontend.log
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success\e[0m"
-else
-    echo -e "\e[31m Fail\e[0m"
-fi
+stat $?
 
 echo -n "restarting nginx:"
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx &>> /tmp/frontend.log
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success\e[0m"
-else
-    echo -e "\e[31m Fail\e[0m"
-fi
+stat $?
