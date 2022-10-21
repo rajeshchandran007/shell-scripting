673932838ead9ce296f1fdfb2cd5292463d59570
Rajeshe <rajeshchandran007@gmail.com>
2022-10-21 14:31:37 +0530

    script change

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -13,8 +13,20 @@ fi
 echo "Installing nginx:"
 yum install nginx -y &>> /tmp/frontend.log
 
+if [ $? -eq 0 ] ; then
+    echo -e "\e[31m Success\e[0m"
+else
+    echo -e "\e[32m Fail\e[0m"
+fi
+
 echo "Downloading project components:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+if [ $? -eq 0 ] ; then
+    echo -e "\e[31m Success\e[0m"
+else
+    echo -e "\e[32m Fail\e[0m"
+fi
 
 echo "Performing cleanup:"
 cd /usr/share/nginx/html
@@ -25,6 +37,18 @@ mv static/* .
 rm -rf frontend-main README.md &>> /tmp/frontend.log
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+if [ $? -eq 0 ] ; then
+    echo -e "\e[31m Success\e[0m"
+else
+    echo -e "\e[32m Fail\e[0m"
+fi
+
 echo "restarting nginx:"
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx &>> /tmp/frontend.log
+
+if [ $? -eq 0 ] ; then
+    echo -e "\e[31m Success\e[0m"
+else
+    echo -e "\e[32m Fail\e[0m"
+fi
