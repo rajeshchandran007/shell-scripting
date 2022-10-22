8131be639953be48c61966641305d9444a7eab7e
Rajeshe <rajeshchandran007@gmail.com>
2022-10-22 11:41:28 +0530

    script change

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
+set -e
 
-echo "I am the catalogue component."
+echo "I am the frontend component."
+
+COMPONENT=catalogue
+APPUSER=roboshop
+
+source components/common.sh
+
+echo -n "Installing nodejs:"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOGFILE
+yum install nodejs -y &>> $LOGFILE
+stat $?
+
+echo -n "Adding User:"
+useradd $APPUSER
+stat $?
+
+echo -n "Downloading project component $COMPONENT:"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+stat $?
+
+echo -n "Moving the component $COMPONENT to $APPUSER home directory"
+cd /home/roboshop
+unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
+mv $COMPONENT-main $COMPONENT
+stat $?
+
+# echo -n "Installing node js component"
+# cd /home/roboshop/$COMPONENT
+# npm install &>> $LOGFILE
+# stat $?
