be7644f3100044c5c316fff8c7f547306e7e13f9
Rajeshe <rajeshchandran007@gmail.com>
2022-10-26 16:46:13 +0530

    script change

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -1,49 +1,49 @@
-#!/bin/bash
-
-echo "I am the mysql component."
+#!/bin/bash 
 
 COMPONENT=mysql
-APPUSER=roboshop
 
 source components/common.sh
 
-echo -n "Configuring the $COMPONENT repo:"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE
-stat $?
+echo -n "Configuring the $COMPONENT Repo:"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE 
+stat $? 
 
 echo -n "Installing $COMPONENT:"
-yum install mysql-community-server -y &>> $LOGFILE
-stat $?
+yum install mysql-community-server -y &>> $LOGFILE 
+stat $? 
 
-echo -n "Start $COMPONENT service:"
+echo -n "Starting $COMPONENT service: "
 systemctl enable mysqld && systemctl start mysqld
 stat $?
 
-echo show databases | mysql -uroot -pRoboShop@1 &>> $LOGFILE
-if [ $? -ne 0 ]
-    echo -n "Changing the default password:"
-    TEMP_PWD=$(grep 'temporary password' /var/log/mysqld.log | awk -F ' ' '{print $NF}')
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p"${TEMP_PWD}" &>> $LOGFILE
-    stat $?
-fi
+echo -n "Changing the default password:"
+DEF_ROOT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' ' '{print $NF}')
 
-echo show plugins | mysql -uroot -pRoboShop@1 | grep validate_password &>> $LOGFILE
-if [ $? -eq 0 ]
-    echo -n "Uninstall the password validate plugin:"
-    uninstall plugin validate_password; | mysql --connect-expired-password -uroot -pRoboShop@1 &>> $LOGFILE
-    stat $?
-fi
+# I only want to change the default password only for the first time.
+# How do I come to know whether the password is changed or not.
+echo show databases | mysql -uroot -p${1} &>> $LOGFILE 
+if [ $? -ne 0 ] ; then 
+    echo -n "Reset Root Password"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${1}';" | mysql  --connect-expired-password  -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE 
+    stat $? 
+fi 
 
-echo -n "Download the $COMPONENT Schema:"
-curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-cd /tmp
+echo show plugins | mysql -uroot -p${1} | grep validate_password; &>> $LOGFILE 
+if [ $? -eq 0 ] ; then 
+    echo -n "Uninstalling Password Validate Plugin: "
+    echo "uninstall plugin validate_password;" | mysql  --connect-expired-password  -uroot -p${1} &>> $LOGFILE 
+    stat $? 
+fi 
+
+echo -n "Downloading the $COMPONENT Schema:"
+cd /tmp 
+curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 unzip -o $COMPONENT.zip &>> $LOGFILE
 stat $?
 
-echo -n "Inject the $COMPONENT Schema:"
-cd $COMPONENT-main
-mysql -u root -pRoboShop@1 < shipping.sql &>> $LOGFILE
-stat $?
+echo -n "Injecting the $COMPONENT Schema:"
+cd /tmp/$COMPONENT-main/
+mysql -uroot -p${1} < shipping.sql &>> $LOGFILE
+stat $? 
 
-echo -e "\e[32m -----$COMPONENT installation completed-----\e[0m"
-
+echo -e "\e[32m __________ $COMPONENT Installation Completed _________ \e[0m"
