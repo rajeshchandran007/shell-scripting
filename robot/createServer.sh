30d949a18e7b8042e7946b8bcaee1d19d31d327e
Rajeshe <rajeshchandran007@gmail.com>
2022-10-31 15:40:49 +0530

    script change

--- robot/createServer.sh
+++ robot/createServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e s/"//g)"
+AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')"
 echo $AMI_ID
