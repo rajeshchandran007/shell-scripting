e9dd2fb0f990ef16821245a9f99924bd5e51ce9c
Rajeshe <rajeshchandran007@gmail.com>
2022-10-31 19:30:54 +0530

    script change

--- robot/createServer.sh
+++ robot/createServer.sh
@@ -11,9 +11,9 @@ AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values
 echo "AMI ID: $AMI_ID"
 
 SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b51-allow-all | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')"
-
 echo "Security Group ID: $SG_ID"
 
 # aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq
+PRIVATE_IP="$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq ."Instances[].PrivateIpAddress" | sed -e 's/"//g')"
+echo "Private IP: $PRIVATE_IP"
