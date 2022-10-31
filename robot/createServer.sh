56349f716281c2b01fcd544b5e68092d59d1df65
Rajeshe <rajeshchandran007@gmail.com>
2022-10-31 15:50:51 +0530

    script change

--- robot/createServer.sh
+++ robot/createServer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')"
 echo $AMI_ID
+
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro
