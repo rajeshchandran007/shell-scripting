951313fbf26868cc5df73caa5ba104850bf26f6b
Rajeshe <rajeshchandran007@gmail.com>
2022-10-31 16:07:43 +0530

    script change

--- robot/createServer.sh
+++ robot/createServer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')"
-echo $AMI_ID
+echo "AMI ID: $AMI_ID"
 
 SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b51-allow-all | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')"
 
-echo $SG_ID
+echo "Security Group ID: $SG_ID"
 
-# aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro | jq
+# aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID | jq
