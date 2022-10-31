4dfd0dd53fe8adb6abe813cac44e4762e29c74cb
Rajeshe <rajeshchandran007@gmail.com>
2022-10-31 16:52:19 +0530

    script change

--- robot/createServer.sh
+++ robot/createServer.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then 
+    echo -e "\e[31m Component name is required \n example usage is: \n\t bash createServer.sh componentName \e[0m"   
+    exit 1 
+fi 
+
+COMPONENT=$1
+
 AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')"
 echo "AMI ID: $AMI_ID"
 
@@ -6,4 +14,6 @@ SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b51-a
 
 echo "Security Group ID: $SG_ID"
 
-# aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID | jq
+# aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'
+
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq
